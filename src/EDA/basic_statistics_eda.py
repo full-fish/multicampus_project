53bf6393f7c0b648e3a8447e8945a91b59f88282
No-answerday <103479486+No-answerday@users.noreply.github.com>
2025-12-29 14:30:38 +0900

    don't save datafiles, one json

--- src/EDA/basic_statistics_eda.py
+++ src/EDA/basic_statistics_eda.py
@@ -31,7 +31,7 @@ class BasicStatsConfig:
     review_cnt_bins: Tuple[int, ...] = (1, 2, 3, 5, 10, 20, 50, 100, 200, 500, 1000)
     valid_scores: Tuple[int, ...] = (1, 2, 3, 4, 5)
 
-    save_outputs: bool = True
+    save_outputs: bool = False
     output_dirname: str = "eda_outputs"
     save_format: str = "parquet"  # "parquet" or "csv"
 
